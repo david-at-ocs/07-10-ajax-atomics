c0977f4ad33cd63906487f037d27b9830bf204d3
David Gardetti <dgardetti@gmail.com>
2015-07-11 15:35:32 -0500

    03 exercise 2 done, but had to use Sumeet's solution.

--- 03/public/global.js
+++ 03/public/global.js
@@ -1,18 +1,14 @@
-var req = new XMLHttpRequest();
-
-req.open("get", "/path1");
-
-req.addEventListener("load", function(){
-  alert(this.response);
-});
-
-
-
-//  ---------------------------------------------------------------------------
-
 var myLink = document.getElementById("clicker");
 
 var handleTheClick = function(event){
+  var req = new XMLHttpRequest();
+
+  req.open("get", this.getAttribute("href"));
+
+  req.addEventListener("load", function(){
+    alert(this.response);
+  });
+
   req.send();
   
   // Prevent the link from refreshing the DOM.
