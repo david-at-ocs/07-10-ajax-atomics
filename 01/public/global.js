cc4404124e4d541bafbcdab7047afb11af62a4b0
David Gardetti <dgardetti@gmail.com>
2015-07-11 12:24:45 -0500

    01 done.  Answering questions.

--- 01/public/global.js
+++ 01/public/global.js
@@ -1,14 +1,19 @@
 var req = new XMLHttpRequest();
 
 // For 'Exercise 2', you will modify this line:
-req.open("get", "/info2");
+req.open("get", "/info1");
 
 req.addEventListener("loadstart", function(){
   console.log("loadstart");
 });
 
 req.addEventListener("load", function(){
-  alert(this.response)
+ 
+  if (this.response == "yes") {
+    alert("yay!");
+  } else {
+    alert("aw, shucks");
+  }
 });
 
 req.send();
